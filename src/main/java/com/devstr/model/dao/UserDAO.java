6feb4e78f91aae4dee42b50a58d6e3f7133b4847
Vladislav Matskevich <vladismat@gmail.com>
2018-11-18 22:16:56 +0200

    [DEVSTR-30] UserDAO Implementation
    Added 2 of 5 methods needed, added javadoc

--- src/main/java/com/devstr/model/dao/UserDAO.java
+++ src/main/java/com/devstr/model/dao/UserDAO.java
@@ -3,13 +3,46 @@ package com.devstr.model.dao;
 import com.devstr.model.User;
 import com.devstr.model.enumerations.UserRole;
 
-import javax.sql.DataSource;
-
 public interface UserDAO {
-    void setDataSource(DataSource dataSource);
-    void createUser(String login, String firstname, String lastname, String email, String password, UserRole userRole);
+    /**
+     * Method writes User to database
+     *
+     * @param login user's login(stores in db as object.name)
+     * @param firstName user's first name
+     * @param lastName user's last name
+     * @param email user's email
+     * @param password user's password(encrypted)
+     * @param userRole user's role
+     */
+    void createUser(String login, String firstName, String lastName, String email, String password, UserRole userRole);
+
+    /**
+     * Get user by id from the DB
+     *
+     * @param id object_id of user in table "Objects" of the DB
+     * @return User with certain id
+     */
     User readUserById(int id);
+
+    /**
+     * Get user with a certain login from the DB
+     *
+     * @param login login of a user in the system(objects.name attribute in the DB)
+     * @return User with certain login
+     */
     User readUserByLogin(String login);
+
+    /**
+     * Update user in the database, when id got no changes
+     *
+     * @param user updated User instance with the old id
+     */
     void updateUser(User user);
+
+    /**
+     * Sets user into inactive mode
+     *
+     * @param user User instance that must be deactivated
+     */
     void inactivateUser(User user);
 }
--- src/main/java/com/devstr/model/dao/impl/UserDAOimpl.java
+++ src/main/java/com/devstr/model/dao/impl/UserDAOimpl.java
@@ -6,52 +6,67 @@ import com.devstr.model.enumerations.UserRole;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
+import java.time.LocalDate;
+import java.util.Date;
 
 public class UserDAOimpl implements UserDAO {
     private DataSource dataSource;
     private JdbcTemplate jdbcTemplate;
-    @Override
+    private static final String INSERT_ATTRIBUTE = "INSERT INTO ATTRIBUTES(ATTRN_ID, OBJECT_ID, VALUE) VALUES(?, ?, ?)";
+    private static final String SELECT_OBJECT_NAME = "SELECT NAME FROM OBJECTS WHERE OBJECT_ID = ?";
+    private static final String SELECT_OBJECT_ID = "SELECT OBJECT_ID FROM OBJECTS WHERE NAME = ?";
+    private static final String UPDATE_OBJECT = "UPDATE OBJECTS SET ? = ? WHERE OBJECT_ID = ?";
+    private static final String UPDATE_ATTRIBUTE = "UPDATE ATTRIBUTES SET ? = ? WHERE OBJECT_ID = ? AND ATTRN_ID = ?";
+    private static final String SELECT_LIST_ID = "SELECT LIST_VALUE_ID FROM LISTS WHERE VALUE = ?";
+    private static final String SELECT_ATTRIBUTE = "SELECT VALUE FROM ATTRIBUTES WHERE OBJECT_ID = ? AND ATTRN_ID = ?";
+
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
-        jdbcTemplate = new JdbcTemplate(dataSource);
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     @Override
-    public void createUser(String login, String firstname, String lastname, String email, String password, UserRole userRole) {
+    public void createUser(String login, String firstName, String lastName, String email, String password, UserRole userRole) {
         String obSQL = "INSERT INTO OBJECTS(NAME, OBJECT_TYPE_ID) VALUES(?, 1)";
         jdbcTemplate.update(obSQL, login);
-        String atrSQL = "INSERT INTO ATTRIBUTES(ATTRN_ID, OBJECT_ID, VALUE) VALUES(?, ?, ?)";
-        int object_id = jdbcTemplate.queryForObject("SELECT OBJECT_ID FROM OBJECTS WHERE NAME = ?",
+        int object_id = jdbcTemplate.queryForObject(SELECT_OBJECT_ID,
                 new Object[]{login}, Integer.class);
-        jdbcTemplate.update(atrSQL, 1, object_id, firstname);;
-        jdbcTemplate.update(atrSQL, 2, object_id, lastname);
-        jdbcTemplate.update(atrSQL, 3, object_id, email);
+        jdbcTemplate.update(INSERT_ATTRIBUTE, 1, object_id, firstName);;
+        jdbcTemplate.update(INSERT_ATTRIBUTE, 2, object_id, lastName);
+        jdbcTemplate.update(INSERT_ATTRIBUTE, 3, object_id, email);
         jdbcTemplate.update("INSERT INTO ATTRIBUTES (ATTRN_ID, OBJECT_ID, DATE_VALUE) VALUES (5, ?, SYSDATE)", object_id);
-        jdbcTemplate.update(atrSQL, 6, object_id, password);
+        jdbcTemplate.update(INSERT_ATTRIBUTE, 6, object_id, password);
         String roleSQL = "INSERT INTO ATTRIBUTES(ATTRN_ID, OBJECT_ID, LIST_VALUE_ID) VALUES(4, ?, ?)";
         int roleId = jdbcTemplate.queryForObject("SELECT LIST_VALUE_ID FROM LISTS WHERE VALUE = ?",
                 new Object[]{userRole.getUserRole()}, Integer.class);
-        jdbcTemplate.update(atrSQL, 7, object_id, 1);
+        jdbcTemplate.update(INSERT_ATTRIBUTE, 7, object_id, 1);
         jdbcTemplate.update(roleSQL, object_id, roleId);
     }
 
     @Override
     public User readUserById(int id) {
+        if (id <= 0) {
+            throw new IllegalArgumentException("ID cannot be 0 or less");
+        }
+        if (id > jdbcTemplate.getMaxRows()){
+            throw new IllegalArgumentException("ID is bigger than size of the table");
+        }
         User.UserBuilder result = User.builder();
-        String username = jdbcTemplate.queryForObject("SELECT NAME FROM OBJECTS WHERE OBJECT_ID = ?",
+        String username = jdbcTemplate.queryForObject(SELECT_OBJECT_NAME,
                 new Object[]{id}, String.class);
         result.setUserId(id);
         result.setLogin(username);
-        String atrSQL = "SELECT VALUE FROM ATTRIBUTES WHERE OBJECT_ID = ? AND ATTRN_ID = ?";
-        result.setFirstName(jdbcTemplate.queryForObject(atrSQL, new Object[]{id, 1}, String.class));
-        result.setLastName(jdbcTemplate.queryForObject(atrSQL, new Object[]{id, 2}, String.class));
-        result.setEmail(jdbcTemplate.queryForObject(atrSQL, new Object[]{id, 3}, String.class));
-        result.setPassword(jdbcTemplate.queryForObject(atrSQL, new Object[]{id, 5}, String.class));
-        int userRoleId = jdbcTemplate.queryForObject(atrSQL, new Object[]{id, 4}, Integer.class);
+        result.setFirstName(jdbcTemplate.queryForObject(SELECT_ATTRIBUTE, new Object[]{id, 1}, String.class));
+        result.setLastName(jdbcTemplate.queryForObject(SELECT_ATTRIBUTE, new Object[]{id, 2}, String.class));
+        result.setEmail(jdbcTemplate.queryForObject(SELECT_ATTRIBUTE, new Object[]{id, 3}, String.class));
+        result.setPassword(jdbcTemplate.queryForObject(SELECT_ATTRIBUTE, new Object[]{id, 6}, String.class));
+        int userRoleId = jdbcTemplate.queryForObject(SELECT_ATTRIBUTE, new Object[]{id, 4}, Integer.class);
         String userRole = jdbcTemplate.queryForObject("SELECT VALUE FROM LISTS WHERE LIST_VALUE_ID = ?",
                 new Object[]{userRoleId}, String.class);
         result.setRole(UserRole.valueOf(userRole));
-        result.setStatus(jdbcTemplate.queryForObject(atrSQL, new Object[]{id, 5}, Boolean.class));
+        result.setHireDate(jdbcTemplate.queryForObject("SELECT DATE_VALUE FROM ATTRIBUTES" +
+                " WHERE OBJECT_ID=? AND ATTRN_ID=?", new Object[]{id, 5}, LocalDate.class));
+        result.setStatus(jdbcTemplate.queryForObject(SELECT_ATTRIBUTE, new Object[]{id, 7}, Boolean.class));
         return result.build();
     }
 
@@ -64,11 +79,23 @@ public class UserDAOimpl implements UserDAO {
 
     @Override
     public void updateUser(User user) {
-
+        jdbcTemplate.update(UPDATE_OBJECT,  "NAME", user.getLogin());
+        jdbcTemplate.update(UPDATE_ATTRIBUTE, "VALUE", user.getFirstName(), user.getUserId(), 1);
+        jdbcTemplate.update(UPDATE_ATTRIBUTE, "VALUE", user.getLastName(), user.getUserId(), 2);
+        jdbcTemplate.update(UPDATE_ATTRIBUTE, "VALUE", user.getEmail(), user.getUserId(), 3);
+        int userRoleId = jdbcTemplate.queryForObject(SELECT_LIST_ID, new Object[]{user.getRole().toString()},
+                Integer.class);
+        jdbcTemplate.update(UPDATE_ATTRIBUTE, "LIST_VALUE_ID", userRoleId, user.getUserId(), 4);
+        jdbcTemplate.update(UPDATE_ATTRIBUTE, "DATE_VALUE", user.getHireDate(), user.getUserId(), 5);
+        jdbcTemplate.update(UPDATE_ATTRIBUTE, "VALUE", user.getPassword(), user.getUserId(), 6);
+        if(user.isStatus())
+            jdbcTemplate.update(UPDATE_ATTRIBUTE, "LIST_VALUE_ID", 6, user.getUserId(), 7);
+        else
+            jdbcTemplate.update(UPDATE_ATTRIBUTE, "LIST_VALUE_ID", 7, user.getUserId(), 7);
     }
 
     @Override
     public void inactivateUser(User user) {
-
+        jdbcTemplate.update(UPDATE_ATTRIBUTE, "LIST_VALUE_ID", 7, user.getUserId(), 7);
     }
 }
